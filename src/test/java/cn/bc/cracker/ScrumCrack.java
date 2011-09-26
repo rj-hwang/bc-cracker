2c7eee6c45aecbdb5fcc2e238955af7a95a890c2
rongjihuang <rongjihuang@gmail.com>
2011-09-26 17:32:05 +0800

    init

---
+++ src/test/java/cn/bc/cracker/ScrumCrack.java
@@ -0,0 +1,12 @@
+package cn.bc.cracker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScrumCrack {
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
